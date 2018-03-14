90584ab7669391189aa9fe83e154ffb6ddd8b407
danoseun <ayodejisomefun@yahoo.com>
2018-03-14 20:27:41 +0100

    implement functionality to allow users see the reviews for a single business

--- server/controllers/review.js
+++ server/controllers/review.js
@@ -33,5 +33,37 @@ class ReviewController {
       message: 'Business not found',
     });
   }
+  /**
+   *  Get review(s) for a business on the platform
+   *
+   * @static
+   * @param {object} req - The request object
+   * @param {object} res - The response object
+   * @return {object} JSON object representing success message
+   * @memberof ReviewController
+   */
+  static getAllReviews(req, res) {
+    for (let i = 0; i < businesses.length; i += 1) {
+      const business = businesses[i],
+        review = [];
+      if (business.id === parseInt(req.params.businessId, 10)) {
+        for (let j = 0; j < reviews.length; j += 1) {
+          if (reviews[j].businessId === business.id) {
+            review.push(reviews[j]);
+          }
+        }
+        return res.status(200).send({
+          message: 'Success, review found',
+          business: businesses[i],
+          reviews: review
+        });
+      }
+    }
+    return res.status(404).send({
+      status: 'Fail',
+      message: 'Business not found'
+    });
+  }
 }
+
 export default ReviewController;
