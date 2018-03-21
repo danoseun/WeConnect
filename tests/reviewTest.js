2591fc39ecfadab830507d295a28aa1ff8ffc514
danoseun <ayodejisomefun@yahoo.com>
2018-03-21 12:47:52 +0100

    write test for review route

---
+++ tests/reviewTest.js
@@ -0,0 +1,70 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from './../app';
+import reviews from '../mockData/review';
+
+const { should, assert, expect } = chai;
+should();
+
+chai.use(chaiHttp);
+
+describe('Test for review routes', () => {
+  describe('POST /api/v1/businesses/:businessId/reviews request route', () => {
+    it('It should return 201 status code and create a review for a business', (done) => {
+      chai.request(app)
+        .post('/api/v1/businesses/2/reviews')
+        .send({
+          id: 1,
+          businessId: 1,
+          content: 'The best fumigators in town!',
+        })
+        .end((err, res) => {
+          res.should.have.status(201);
+          res.body.should.be.an('object');
+          expect(res.body.status).to.equal('Success');
+          expect(res.body.message).to.equal('Review added successfully');
+          expect(res.body).to.have.property('review');
+          done();
+        });
+    });
+  });
+  it('It should return 404 staus code for business not found in the database', () => {
+    chai.request(app)
+      .post('/api/v1/businesses/15/reviews')
+      .send(reviews.review15)
+      .end((err, res) => {
+        res.should.have.status(404);
+        res.body.should.be.a('object');
+        expect(res.body.status).to.equal('Fail');
+        expect(res.body.message).to.equal('Business not found');
+      });
+  });
+  describe('GET /api/v1/businesses/2/reviews request route', () => {
+    it('It should return 200 status code and get all reviews for a business', (done) => {
+      chai.request(app)
+        .get('/api/v1/businesses/2/reviews')
+        .send(reviews.review1)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          expect(res.body.message).to.equal('Success, review found');
+          expect(res.body).to.have.property('business');
+          expect(res.body).to.have.property('reviews');
+          done();
+        });
+    });
+  });
+  it('It should return 404 for a business not found', (done) => {
+    chai.request(app)
+      .get('/api/v1/businesses/15/reviews')
+      .send(reviews.review1)
+      .end((err, res) => {
+        res.should.have.status(404);
+        res.body.should.be.a('object');
+        expect(res.body.status).to.equal('Fail');
+        expect(res.body.message).to.equal('Business not found');
+        done();
+      });
+  });
+});
+
