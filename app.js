6ce9a35923e621407e171949791c34a88764385e
danoseun <ayodejisomefun@yahoo.com>
2018-04-06 17:27:42 +0100

    document apis

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ import express from 'express';
 import expressValidator from 'express-validator';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 import routes from './server/routes';
 
 // Setup the express app
 const app = express();
+const swaggerDocument = YAML.load(`${process.cwd()}/swagger.yaml`);
 
 // Log requests to the console
 app.use(logger('dev'));
@@ -17,6 +20,7 @@ app.use(expressValidator());
 
 // Require the routes
 app.use('/api/v1', routes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Catch other routes that don't exist
 app.all('*', (req, res) => res.status(404).send({
